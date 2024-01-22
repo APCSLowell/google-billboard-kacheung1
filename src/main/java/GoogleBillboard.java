d599a0e3798c039494b5d82bab89acce868976ce
mrchanr <42151767+mrchanr@users.noreply.github.com>
2024-01-22 14:39:17 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,8 +1,10 @@
 public class GoogleBillboard {
 	public final static String e = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
+		setup();
+	}
       
-public void setup()  
+public static void setup()  
 {            
 
    for (int i=0; i<e.length()-10; i++)
@@ -34,6 +36,5 @@ if (dNum%i==0)
 
     return true;  
 } 
-}
 }
 
